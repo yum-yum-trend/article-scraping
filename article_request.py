41af981eae37725e9fe009731ac8ab36d9c1e9dd
hellonayeon <hellonykwon@gmail.com>
2021-12-02 02:08:39 +0900

    이미지 파일 업로드 성공 / 뒤죽박죽 코드 #5

--- article_request.py
+++ article_request.py
@@ -1,5 +1,7 @@
 import os
 import json
+from urllib.request import urlopen
+
 from PIL import Image
 from io import BytesIO
 
@@ -46,54 +48,57 @@ def get_data():
                     "yCoordinate":  location["y"],
                     "categoryName":  location["category_name"]
                 }
-                #
-                # for i in data:
-                #     url = i['IMG_URL']  # img URL만 뽑아옴
-                #
-                #     r = requests.get(url, stream=True)
-                #     session = boto3.Session()
-                #     s3 = session.resource('s3')
-                #
-                #     bucket_name = os.environ["BUCKET_NAME"]  # 버킷이름
-                #     file_name = url.split("/")[-1]  # 파일이름
-                #     key = f'folder1/{file_name}'  # 폴더명/파일이름
-                #
-                #     bucket = s3.Bucket(bucket_name)
-                #     bucket.upload_fileobj(r.raw, key)
-
-
 
             image_files = {
                 "imageFiles": []
             }
 
-            image_urls = article_data[row_num]["imageFiles"]
-            for image_url in image_urls:
-                r = requests.get(image_url, stream=True)
-                # response = requests.get(image_url)
-                # image = Image.open(BytesIO(response.content))
-                # image_files["imageFiles"].append(image)
-                image_files["imageFiles"].append(r.raw)
+            # image_urls = article_data[row_num]["imageFiles"]
+            # for image_url in image_urls:
+            #     r = requests.get(image_url, stream=True)
+            #     # response = requests.get(image_url)
+            #     # image = Image.open(BytesIO(response.content))
+            #     # image_files["imageFiles"].append(image)
+            #     image_file = Image.open(requests.get(image_url, stream=True).raw)
+            #     image_files.append(image_file)
 
             payload = {
                 "text": text,
                 "tagNames": tag_names,
                 "location": json.dumps(location_info),
-                "imageFiles": image_files
+
             }
 
+            image_url = article_data[row_num]["imageFiles"][0]
+            print(image_url)
+            # r = requests.get(image_url, stream=True)
+            # payload["imageFiles"] = Image.open(requests.get(image_url, stream=True).raw)
+            # image_files["imageFiles"] = Image.open(requests.get(image_url, stream=True).raw)
+
+
+
+            response = requests.get(image_url)
+            print(response)
+            image = BytesIO(response.content)
+            image_files["imageFiles"].append(image)
+            image_files["imageFiles"].append(image)
+            print(image)
+
+            tmp_image_files = [
+                               ('imageFiles', ('a.png', response.content, 'image/png')),
+                               ('imageFiles', ('a.png', response.content, 'image/png'))]
 
             print(payload)
 
             header = {
-                "Content-Type": 'application/json',
+                # "Content-Type": "multipart/form-data;",
                 # 'Content-Disposition': 'form-data',
                 "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJOYXllb25Ld29uIiwiZXhwIjoxNjM4Mzc5NjY4LCJpYXQiOjE2MzgzNjE2Njh9.2cWiLjFltgMAO6knV72Sp6nL_x5TQz1xkLWbipKMB0fnckoKH0bzmbocvC7fvKE95o8kFPSpTLHHRCNgOmk16w"
             }
 
             # response = requests.post('http://localhost:8080/articles', files=multipart_form_data)
-            session = requests.Session()
-            response = session.post('http://localhost:8080/articles/scraping', headers=header, data=payload)
+            # session = requests.Session()
+            response = requests.post('http://localhost:8080/articles', headers=header, files=tmp_image_files, data=payload)
 
             print("########## response ##########")
             print(response)
