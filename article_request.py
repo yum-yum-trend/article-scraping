e88a83c12d75527b9554df560067615d2e8e1ae2
hellonayeon <hellonykwon@gmail.com>
2021-12-28 22:38:03 +0900

    이미지 API 분리에 따른 요청 수정

--- article_request.py
+++ article_request.py
@@ -1,12 +1,15 @@
 import os
 import json
 import time
+import random
 
 import requests
 from kakao_map_api import get_place
 
 files = []
-articles = []
+tokens = [
+    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuYXllb24iLCJleHAiOjE2NDA5NTQ5MTcsImlhdCI6MTY0MDY5NTcxN30.jsKdLp2RPJmAu3470_j9ywsaQ29Uue7wH17wgAx7pqd7JibXzN9n2RgCZgqhGvIjjTKc6-IwIcawCdVUeCn2NQ"
+]
 
 
 # 폴더 하위의 (JSON) 파일 이름들 가져오기
@@ -18,58 +21,71 @@ def find_files(dir_name):
 
 
 def get_data():
-    for file in files:
+    file = files[0]
+    # for file in files:
 
-        with open(file, 'r') as f:
-            article_data = json.load(f)  # json_data = dict
+    with open(file, 'r') as f:
+        article_data = json.load(f)  # json_data = dict
 
-        for row_num in article_data:
-            cnt = cnt + 1
-            place_name = article_data[row_num]['location']
-            location = get_place(place_name)
-            time.sleep(1)
+    for row_num in article_data:
+        place_name = article_data[row_num]['location']
+        location = get_place(place_name)
+        time.sleep(1)
 
-            text = article_data[row_num]['text']
-            tag_names = []
-            for tagStr in article_data[row_num]['tagNames']:
-                tag_names = tag_names + tagStr.split(',')
+        text = article_data[row_num]['text']
+        tag_names = []
+        for tagStr in article_data[row_num]['tagNames']:
+            tag_names = tag_names + tagStr.split(',')
 
-            location_info = {}
-            if location:
-                location_info = {
-                    "roadAddressName": location["road_address_name"],
-                    "placeName":  location["place_name"],
-                    "xCoordinate":  location["x"],
-                    "yCoordinate":  location["y"],
-                    "categoryName":  location["category_name"]
-                }
-
-            image_files = []
-            image_urls = article_data[row_num]["imageFiles"]
-            for url in image_urls:
-                response = requests.get(url)
-                file_name = url.split("/")[-1].split("?")[0]
-                print(file_name)
-                image_files.append(('imageFiles', (file_name, response.content, 'image/*')))
-                time.sleep(1)
-
-                # tmp_image_files = [
-                #     ('imageFiles', ('a.png', response.content, 'image/png')),
-                #     ('imageFiles', ('a.png', response.content, 'image/png'))]
-
-            payload = {
-                "text": text,
-                "tagNames": tag_names,
-                "location": json.dumps(location_info),
+        location_info = {}
+        if location:
+            location_info = {
+                "roadAddressName": location["road_address_name"],
+                "placeName": location["place_name"],
+                "xCoordinate": location["x"],
+                "yCoordinate": location["y"],
+                "categoryName": location["category_name"]
             }
 
-            header = {
-                "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJOYXllb25Ld29uIiwiZXhwIjoxNjM4Mzk3ODY1LCJpYXQiOjE2MzgzNzk4NjV9.-v_PMcRXc1iaUHESHFnOLGgfOS3wws991HrKhwIYCST81HbKFKqtKqQMm4rvx4W0RZAjeXhvFdid5rdABgsl9Q"
-            }
+        image_req_header = {
+            "Authorization": f"Bearer {tokens[random.randrange(0, len(tokens))]}"
+        }
 
-            response = requests.post('http://localhost:8080/articles', headers=header, files=image_files, data=payload)
+        image_ids = []
+        image_urls = article_data[row_num]["imageFiles"]
+        for url in image_urls:
+            response = requests.get(url)
+            file_name = url.split("/")[-1].split("?")[0]
+            image_file = {'imageFile': (file_name, response.content, 'image/*')}
+
+            image_object = requests.post('http://localhost:8080/article/image', files=image_file, headers=image_req_header)
+            image = image_object.json()
+            image_ids.append(image['id'])
 
             time.sleep(3)
+
+            # tmp_image_files = [
+            #     ('imageFiles', ('a.png', response.content, 'image/png')),
+            #     ('imageFiles', ('a.png', response.content, 'image/png'))]
+        print(f'image_ids={image_ids}')
+
+        payload = {
+            "text": text,
+            "tagNames": tag_names,
+            "location": json.dumps(location_info),
+            "imageIds": image_ids
+        }
+
+        article_req_header = {
+            "Content-type": "application/json",
+            "Authorization": f"Bearer {tokens[random.randrange(0, len(tokens))]}"
+        }
+
+        response = requests.post(
+            'http://localhost:8080/articles', headers=article_req_header, data=json.dumps(payload))
+
+        print(response)
+        print(response.json())
 
 
 find_files('json_data')
